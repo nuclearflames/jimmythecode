f724440219124fc38da961df41680b8788fe7fd8
James Grant <1993jamesgrant@gmail.com>
2014-07-07 00:39:39 +0100

    improved mimes

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -3,5 +3,6 @@
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
 
-Mime::Type.register "application/woff", :woff
-Mime::Type.register "application/svg", :svg
+Mime::Type.register "application/x-font-woff", :woff
+Mime::Type.register "application/x-font-ttf", :ttf
+Mime::Type.register "image/svg+xml", :svg
